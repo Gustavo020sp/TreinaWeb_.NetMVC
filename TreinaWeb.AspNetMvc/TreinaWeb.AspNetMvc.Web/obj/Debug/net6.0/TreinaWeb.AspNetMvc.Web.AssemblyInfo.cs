266afda1e7282d55d717762d2a20598d3e6bc86d
Gustavo Gomes <gustavo020sp@gmail.com>
2024-04-09 21:57:55 -0300

    Sending info from Controllers to Views

--- TreinaWeb.AspNetMvc/TreinaWeb.AspNetMvc.Web/obj/Debug/net6.0/TreinaWeb.AspNetMvc.Web.AssemblyInfo.cs
+++ TreinaWeb.AspNetMvc/TreinaWeb.AspNetMvc.Web/obj/Debug/net6.0/TreinaWeb.AspNetMvc.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreinaWeb.AspNetMvc.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06a65e60c688da7c3a13d46cf92fdf4400b34fb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreinaWeb.AspNetMvc.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreinaWeb.AspNetMvc.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
