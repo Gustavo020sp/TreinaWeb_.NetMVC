1a8671028d4f215d7a6ab36f2d5dd0504229e780
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2024-04-09 22:00:12 -0300

    Controllers and View changes

---
+++ Controllers/TesteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TreinaWeb.AspNetMvc.Web.Controllers
+{
+    public class TesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //Metodo abaixo vai ser chamado pelo BeginForm, na view Index
+        [HttpPost]
+		public IActionResult Index(IFormCollection iformCollection)
+		{
+            string name = iformCollection["Username"];
+			ViewBag.Name = name;
+			return View("Views/Teste/Greetings.cshtml");
+            //comment here
+		}
+	}
+}
