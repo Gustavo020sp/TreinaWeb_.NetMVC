fef5367e45d27574585611a321fc319bcd65fff9
Gustavo Gomes <gustavo020sp@gmail.com>
2024-04-11 17:38:08 -0300

    update

--- TreinaWeb.AspNetMvc/TreinaWeb.AspNetMvc.Web/obj/Debug/net6.0/TreinaWeb.AspNetMvc.Web.AssemblyInfo.cs
+++ TreinaWeb.AspNetMvc/TreinaWeb.AspNetMvc.Web/obj/Debug/net6.0/TreinaWeb.AspNetMvc.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreinaWeb.AspNetMvc.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fec85ebea2fd99b7fd120c416f97ca01dbb0074f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3adc8017f9f52c51854c71c8135fb7be9216d306")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreinaWeb.AspNetMvc.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreinaWeb.AspNetMvc.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
