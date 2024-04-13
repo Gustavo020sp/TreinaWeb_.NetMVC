62c385663edab42c3f78bc6ce02ffd83e577629c
Gustavo Gomes <gustavo020sp@gmail.com>
2024-04-12 21:02:52 -0300

    Desenvolvendo uma aplicação com o ASP.NET MVC em várias camadas independentes e reutilizáveis

---
+++ TreinaWeb.Musics/TreinaWeb.Musics.Domain/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinaWeb.Musics.Domain
+{
+	public class Album
+	{
+        //POCO
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string Observations { get; set; }
+    }
+}
